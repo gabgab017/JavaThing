425f9e204a1201e9c19234dada6c03f1e0e82537
Gabor Szijj <000464710@student.vcc.ca>
2023-01-10 11:21:57 -0800

    gabor's

---
+++ Java01/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String firstname = "Gab", lastname = "Szijj";
+        int age = 34;
+        boolean isCool = true;
+
+        System.out.println("My name is " + firstname + lastname + "and my age is" + age + "and it is" + isCool);
+
+
+
+    }
+}
